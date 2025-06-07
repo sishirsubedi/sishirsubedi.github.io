98e80a77bbf8a54853da901859d2703485d20037
sishirsubedi <SUBEDISISHIR@GMAIL.COM>
2025-06-07 07:24:51 +0000

    Deploying to gh-pages from @ sishirsubedi/sishirsubedi.github.io@7c38acfaac85cf50fcc21cdbdbc4ceba0272da9d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,6 +72,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-one-liner-ideas",
+          title: 'One-liner ideas',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-email',
         title: 'email',
