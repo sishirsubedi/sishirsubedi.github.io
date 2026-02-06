11216acbc319b8b6bfd1d63102f519a6adaaf66a
sishirsubedi <SUBEDISISHIR@GMAIL.COM>
2026-02-06 12:36:36 +0000

    Deploying to gh-pages from @ sishirsubedi/sishirsubedi.github.io@54515d521e415dc65d6f2a2ca4dd994b8aef9eb6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,7 +52,12 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
-      },{id: "projects-grasp",
+      },{id: "projects-study-spamosaic-model",
+          title: 'Study SpaMosaic model',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/10_project/";
+            },},{id: "projects-grasp",
           title: 'GRASP',
           description: "",
           section: "Projects",handler: () => {
